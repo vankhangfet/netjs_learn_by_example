cf2c3f66fbc5ff957973273283ce23582fbabdf2
Nguyen Van Khang <vankhangfet@gmail.com>
2022-06-11 23:06:36 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './module/productsmodule.module';
 
 @Module({
   imports: [ TypeOrmModule.forRoot({
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
       autoLoadEntities: true,
-  })],
+  }),
+   ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
